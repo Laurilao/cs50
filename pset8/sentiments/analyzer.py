9c7dc718847f4685a1c511f406e69d7f400f3dc6
Olli Laurila <olli451@gmail.com>
2017-09-15 15:24:17 +0000

    pset8 done

--- pset8/sentiments/analyzer.py
+++ pset8/sentiments/analyzer.py
@@ -6,10 +6,37 @@ class Analyzer():
     def __init__(self, positives, negatives):
         """Initialize Analyzer."""
 
-        # TODO
-
+        # add words from positive-words.txt
+        self.positives = set()
+        file = open("positive-words.txt", "r")
+        for line in file:
+            if not line.startswith(";"):
+                self.positives.add(line.strip())
+        file.close()
+        
+        # add words from negative-words.txt
+        self.negatives = set()
+        file = open("negative-words.txt", "r")
+        for line in file:
+            if not line.startswith(";"):
+                self.negatives.add(line.strip())
+        file.close()
+        
     def analyze(self, text):
         """Analyze text for sentiment, returning its score."""
-
-        # TODO
-        return 0
+        
+        score = 0
+        
+        tokenizer = nltk.tokenize.TweetTokenizer()
+        tokens = tokenizer.tokenize(text)
+        
+        for token in tokens:
+            token.lower()
+            if token in self.positives:
+                score += 1
+            elif token in self.negatives:
+                score -= 1
+            else:
+                score += 0
+        
+        return score
